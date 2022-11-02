2a0410c7ef29aa47811fccd639b6410ea70bfcef
anadantasp <caroldantasp@gmail.com>
2022-11-02 12:33:18 -0300

    Combinando CSS selector com texto

--- buger-eats/cypress/e2e/cadastro.cy.js
+++ buger-eats/cypress/e2e/cadastro.cy.js
@@ -19,7 +19,8 @@ describe('Cadastro', ()=>{
                 complemento: 'Apto 142',
                 bairro: 'Itaim Bibi',
                 cidade_uf: 'São Paulo/SP'
-            }
+            },
+            metodo_entrega: 'Moto'
         }
 
         cy.get('input[name="name"]').type(entregador.nome)
@@ -35,5 +36,8 @@ describe('Cadastro', ()=>{
         cy.get('input[name="address"]').should('have.value', entregador.endereco.rua)
         cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro)
         cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade_uf)
+
+        cy.contains('.delivery-method li', entregador.metodo_entrega).click()
+
     })
 })
