365664f4e96fcd45bcb0fd8cf276e94b05d1b3f5
Ana Carolina Dantas Prado <rx206@xgen.local>
2022-11-04 11:58:59 -0300

    Submetendo forms e validando modal

--- buger-eats/cypress/e2e/cadastro.cy.js
+++ buger-eats/cypress/e2e/cadastro.cy.js
@@ -42,5 +42,11 @@ describe('Cadastro', ()=>{
 
         cy.get('input[accept^="image"]').attachFile('/images/' + entregador.cnh)
 
+        cy.get('form button[type="submit"]').click()
+
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+
+        cy.get('.swal2-container .swal2-html-container').should('have.text', expectedMessage)
+
     })
 })
