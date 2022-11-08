3779bd828dd20f25984e3b40df6f06bbfa7ea0db
Ana Carolina Dantas Prado <rx206@xgen.local>
2022-11-08 14:38:54 -0300

    Trabalhando com fixtures

--- buger-eats/cypress/e2e/cadastro.cy.js
+++ buger-eats/cypress/e2e/cadastro.cy.js
@@ -2,27 +2,15 @@ import signup from '../pages/SignupPage'
 
 describe('Cadastro', ()=>{
 
-    it('Usuário deve se tornar um entregador', ()=>{
+    beforeEach(function(){
+        cy.fixture('deliver').then((d) => {
+            this.deliver = d 
+        })
+    })
 
-        var deliver = {
-            name: 'Ana Prado',
-            cpf: "00000014141",
-            email: 'anaprado@email.com',
-            whatsapp: '11999999999',
-            address:{
-                postalcode: '04534011',
-                street: 'Rua Joaquim Floriano',
-                number: '1000',
-                details: 'Apto 142',
-                district: 'Itaim Bibi',
-                city_state: 'São Paulo/SP'
-            },
-            delivery_method: 'Moto',
-            cnh: 'cnh-digital.jpg'
-        }
-
+    it('Usuário deve se tornar um entregador', function(){
         signup.go()
-        signup.fillForm(deliver)
+        signup.fillForm(this.deliver.signup)
         signup.submit()
 
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
@@ -31,27 +19,9 @@ describe('Cadastro', ()=>{
 
     })
 
-    it('CPF incorreto', ()=>{
-
-        var deliver = {
-            name: 'Ana Prado',
-            cpf: "000000141AA",
-            email: 'anaprado@email.com',
-            whatsapp: '11999999999',
-            address:{
-                postalcode: '04534011',
-                street: 'Rua Joaquim Floriano',
-                number: '1000',
-                details: 'Apto 142',
-                district: 'Itaim Bibi',
-                city_state: 'São Paulo/SP'
-            },
-            delivery_method: 'Moto',
-            cnh: 'cnh-digital.jpg'
-        }
-
+    it('CPF incorreto', function(){
        signup.go()
-       signup.fillForm(deliver)
+       signup.fillForm(this.deliver.cpf_inv)
        signup.submit()
        signup.alertMessageShouldBe('Oops! CPF inválido')
     })
