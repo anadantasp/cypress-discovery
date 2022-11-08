2d8f874e7fa81efd33f71e6ef7a023ffb6d6dacc
Ana Carolina Dantas Prado <rx206@xgen.local>
2022-11-08 11:12:37 -0300

    Viewport e Base URL

--- buger-eats/cypress.config.js
+++ buger-eats/cypress.config.js
@@ -1,9 +1,23 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: 1440,
+  viewportHeight: 900,
   e2e: {
+    baseUrl: 'https://buger-eats.vercel.app/',
+    reporter: 'mochawesome',
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: 'bugereats',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: true,
+      reportFilename: 'customReportFilename',
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      // mochawesome
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
